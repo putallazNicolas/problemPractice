97a52fffd95d9b280f8c7dabb4e5decac2d5efd8
Nicolás Putallaz <77561348+putallazNicolas@users.noreply.github.com>
2024-12-27 12:05:11 -0300

    problem counter succesfully count files

--- problemCounter/problemCounter.c
+++ problemCounter/problemCounter.c
@@ -4,32 +4,51 @@
 #include <string.h>
 
 void changePath(char *path);
+int countFiles(char *path);
 
 const char *thisPath = "\\problemCounter\\problemCounter.exe";
 
 int main(void){
+    // Get problems path
     char directory[MAX_PATH];
     GetModuleFileName(NULL, directory, MAX_PATH); // Finds where is this program installed
-    changePath(directory);
+    changePath(directory); // Changes the current path to problems path
 
-    printf("%s\n", directory);
+    // Open directory to count files
+    int fileCount = countFiles(directory);
+    printf("%i\n", fileCount);
+    // Change README file
 }
 
 void changePath(char *path) {
-    // Buscar la posición de 'thisPath' en 'path'
     char *pos = strstr(path, thisPath);
     if (pos != NULL) {
-        // Calcular la longitud de la parte de 'path' antes de 'thisPath'
         int prefixLen = pos - path;
         
-        // Calcular la longitud de la parte de 'path' después de 'thisPath'
         int suffixLen = strlen(path) - (prefixLen + strlen(thisPath));
 
-        // Mover el contenido posterior a 'thisPath' hacia el principio del arreglo
         memmove(path + prefixLen, path + prefixLen + strlen(thisPath), suffixLen + 1);
     }
 
-    // Agregar '\\problems' al final de la ruta
-    const char *problemsPath = "\\problems";
+    const char *problemsPath = "\\problems\\*"; // Add * to include every file in the path
     strcat(path, problemsPath);
+}
+
+int countFiles(char *path){
+    int counter = 0; // file counter
+
+    WIN32_FIND_DATA ffd; // ffd stands for find file data
+    HANDLE hFind = FindFirstFile(path, &ffd);
+
+    if (hFind == INVALID_HANDLE_VALUE){ // findFirstFile return that when error
+        printf("Couldnt find files");
+    }
+    else {
+        do {
+            if (strcmp(ffd.cFileName, ".") != 0 && strcmp(ffd.cFileName, "..") != 0) {
+                counter++;
+            }
+        } while (FindNextFile(hFind, &ffd) != 0);
+    }
+    return counter;
 }
